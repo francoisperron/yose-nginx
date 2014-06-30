647e4518a25b5e68d0fbb413ff750d942f64dcf6
Francois Perron <fperron@gmail.com>
2014-06-29 23:23:47 -0400

    ngnix.conf with variables to start localy and on openshift

---
+++ local-load-conf.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp -a nginx.conf /usr/local/conf/nginx.conf
+sed -i.bak "s/NGINX_IP_PORT/localhost:80/" /usr/local/conf/nginx.conf
+sed -i.bak "s/YOSE_CLIENT/localhost:5001/g" /usr/local/conf/nginx.conf
+sed -i.bak "s/YOSE_NODE/localhost:5000/g" /usr/local/conf/nginx.conf
---
+++ local-start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+nginx > /usr/local/logs/server.log 2>&1 &
